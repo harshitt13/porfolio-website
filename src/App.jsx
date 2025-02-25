fedbed494976df0394fc1dac1165acacd50ee582
harshitt13 <hk9084760388@gmail.com>
2025-02-25 23:43:24 +0530

    updated tailwindcss v4.0.0-alpha.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import Navbar from './components/Navbar';
+import './App.css'
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-      <Navbar />
-      {/* Add other components or pages here */}
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-bold">Hello, Tailwind CSS v4!</h1>
     </div>
   );
-};
-
-export default App;
+}
